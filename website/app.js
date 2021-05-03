94b3e88db859fba5f23f581c48225aba178016f9
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2021-05-03 04:54:23 +0200

    fix If condition

--- website/app.js
+++ website/app.js
@@ -8,16 +8,16 @@ const zipCode = document.getElementById("zip");
 let felingText= document.getElementById("feelings").value;
 // Personal API Key for OpenWeatherMap API
 const apiKey = "db946a07f453749c571d0f55fe825d5b";
-// Base URL
-// const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=metric`;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 // useing click event listener on the Generate Button
 btn.addEventListener('click', handlBtnClk);
 function handlBtnClk(){
+    // const aipCode again to fix the If condition
+    const zipCode = document.getElementById("zip");
     // make acondition to make sure that user enter a value in the text input
-    if( zipCode.value === " "){
+    if( zipCode.value === ""){
         alert("Please Enter a Valid ZIP Code"); 
     } else {
         //start calling our 3 functions
