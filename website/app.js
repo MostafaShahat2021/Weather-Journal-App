de9ea69173efa8f8c4c2e363c338505874222fd6
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2021-05-03 03:42:57 +0200

    after tutor instructions

--- website/app.js
+++ website/app.js
@@ -3,17 +3,16 @@
 // get generate button
 const btn = document.getElementById("generate");
 // get user zip code
-const zipCode = document.getElementById("zip").value;
+const zipCode = document.getElementById("zip");
 // get user fellings
 let felingText= document.getElementById("feelings").value;
 // Personal API Key for OpenWeatherMap API
 const apiKey = "db946a07f453749c571d0f55fe825d5b";
 // Base URL
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=metric";
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=metric`;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
 // useing click event listener on the Generate Button
 btn.addEventListener('click', handlBtnClk);
 function handlBtnClk(){
@@ -25,12 +24,12 @@ function handlBtnClk(){
         getData()
     }
 }
-
 const getData = async () => {
-    const res = await fetch(apiUrl)
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=metric`)
     try{
         const data = await res.json()
-    return
+     console.log(data)
+     return
     } catch (error) {
         console.log(error);
     }
