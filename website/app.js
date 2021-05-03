16ebc0f22979365da2dfc4e4e26e3239d6d123e0
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2021-05-03 04:00:28 +0200

    remove apiUrl

--- website/app.js
+++ website/app.js
@@ -9,7 +9,7 @@ let felingText= document.getElementById("feelings").value;
 // Personal API Key for OpenWeatherMap API
 const apiKey = "db946a07f453749c571d0f55fe825d5b";
 // Base URL
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=metric`;
+// const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=metric`;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
